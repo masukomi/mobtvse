1249dc292113a46fa3913c1d103321cb213728fb
masukomi <masukomi@masukomi.org>
2012-04-29 04:45:08 -0400

    converted to mongoid

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,15 @@
-class Post < ActiveRecord::Base
+class Post
+  include Mongoid::Document
+  include Mongoid::MultiParameterAttributes
+
+  field :title,         :type => String
+  field :text,          :type => String
+  field :draft,         :type => Boolean,   default: true
+  field :aside,         :type => Boolean,   default: true
+  field :url,           :type => String
+  field :updated_at,    :type => DateTime
+  field :created_at,    :type => DateTime
+
   validates :title, presence: true
   validates :slug, presence: true, uniqueness: true
   acts_as_url :title, :url_attribute => :slug
