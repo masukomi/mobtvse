ad3e2b48f57e57b906c8172c1c8479389bc5644d
masukomi <masukomi@masukomi.org>
2012-05-02 08:42:28 -0400

    now autoloading lib directory. solves stringex problem

--- config/application.rb
+++ config/application.rb
@@ -22,6 +22,7 @@ module Obtvse
 
     # Custom directories with classes and modules you want to be autoloadable.
     # config.autoload_paths += %W(#{config.root}/extras)
+    config.autoload_paths += Dir["#{config.root}/lib/**/"]  # include all subdirectories
 
     # Only load the plugins named here, in the order given (default is alphabetical).
     # :all can be used as a placeholder for all plugins not explicitly named.
