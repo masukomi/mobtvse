ebfd4f9a617208ca23e9d3d215879b542a02d1cf
masukomi <masukomi@masukomi.org>
2012-05-07 08:46:17 -0400

    Assorted changes to enable Heroku support
    * moved thin out of the development block for heroku
    * environments/production.rb will have it fall back to assets pipeline
    	This should not be a performance hit at the moment because
    	none of the css is dynamically generated anyway.
    	We may want to disable this in the future
    * added 'sprockets/railtie' to application.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -15,8 +15,8 @@ Obtvse::Application.configure do
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-  config.assets.compile = false #default
-  #config.assets.compile = true
+  #config.assets.compile = false #default
+  config.assets.compile = true
 
   # Generate digests for assets URLs
   config.assets.digest = true
